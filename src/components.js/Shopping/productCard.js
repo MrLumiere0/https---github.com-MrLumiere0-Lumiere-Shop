a35872e2e77b20409a94f15821d0eaf8e1a4a6c0
Hashid <hashidlouis@gmail.com>
2024-02-08 14:51:24 -0500

    clear

--- src/components.js/Shopping/productCard.js
+++ src/components.js/Shopping/productCard.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from "react";
+import React, { useContext } from "react";
 import "../../App.css";
 import rhude from "../../assets/rhude.jpg";
 import { Link } from "react-router-dom";
